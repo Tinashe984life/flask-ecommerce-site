357401646dd41abb0ab16d047986bcd5690c7e3f
Tinashe Madanire <madtinashe@gmail.com>
2022-06-01 00:11:07 +0200

    set up app for heroku

--- store/__init__.py
+++ store/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///store.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://iznbjcptcxrtje:d72e2e6a88737fbe67292926dc13f3f815218c761b2c237981a54a686f083ef4@ec2-54-211-255-161.compute-1.amazonaws.com:5432/d618bte7e6fbug'
 app.config['SECRET_KEY'] = 'supersecret'
 
 db = SQLAlchemy(app)
